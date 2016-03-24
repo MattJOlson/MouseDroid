8fd5a7cecfa5f2813c14655bf9f7415c68440afd
Matt Olson <molson@paybyphone.com>
2016-03-24 10:33:39 -0700

    MouseDroid tests: Switched from karma to mocha

---
+++ test/contracts.js
@@ -0,0 +1,7 @@
+var expect = require('expect');
+
+describe("contracts", function() {
+    it("fails when I want to prove that I can run tests", function() {
+        expect(true).toBe(false);
+    });
+});
