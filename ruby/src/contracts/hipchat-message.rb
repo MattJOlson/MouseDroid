4beba6c763368e585c9f2c3247349a43e016c116
Matt Olson <molson@paybyphone.com>
2016-03-24 17:03:29 -0700

    MouseDroid ruby: Yeah, this is better

--- ruby/src/contracts/hipchat-message.rb
+++ ruby/src/contracts/hipchat-message.rb
@@ -2,11 +2,16 @@ require 'multi_json'
 
 module Contracts
   class HipchatMessage
-    attr_reader :sender, :message
+    attr_reader :sender, :message, :jiraIssues
     def initialize(json)
       message = MultiJson.load(json)['item']['message']
       @sender = message['from']['name']
       @message = message['message']
+      @jiraIssues = @message.scan(/[A-Z]{3,5}-\d+/)
+    end
+
+    def hasJiraIssue
+      !@jiraIssues.empty?
     end
   end
 end
--- ruby/test/contracts-test.rb
+++ ruby/test/contracts-test.rb
@@ -2,9 +2,9 @@ require 'test/unit'
 require 'contracts/hipchat-message'
 
 class TestHipchatMessage < Test::Unit::TestCase
-  @@jiraIssueName = 'THX-1138'
-  @@messageText = "Message containing #{@@jiraIssueName}"
-  @@testReplyJson = <<JSON
+
+  def build_test_json(messageText)
+    return <<JSON
 {
   "event": "event",
   "item": {
@@ -22,19 +22,45 @@ class TestHipchatMessage < Test::Unit::TestCase
           "mention_name": "@MentionedUser"
         }
       ],
-      "message": "#{@@messageText}",
+      "message": "#{messageText}",
       "type": "Message Type"
     }
   }
 }
 JSON
-  def test_sender
-    msg = Contracts::HipchatMessage.new(@@testReplyJson)
+  end
+
+  def test_sender_should_parse_out_message_from_name
+    msg = Contracts::HipchatMessage.new(build_test_json("whatever"))
     assert_equal(msg.sender, "J. Random User")
   end
 
-  def test_message
-    msg = Contracts::HipchatMessage.new(@@testReplyJson)
-    assert_equal(msg.message, @@messageText)
+  def test_message_should_parse_out_message_message
+    msg = Contracts::HipchatMessage.new(build_test_json("whatever"))
+    assert_equal(msg.message, "whatever")
   end
+
+  def test_jira_issue_should_be_absent_when_not_present
+    msg = Contracts::HipchatMessage.new(build_test_json("foobaz"))
+    assert_false(msg.hasJiraIssue)
+  end
+
+  def test_jira_issue_should_be_found_when_present
+    text = "Message containing THX-1138, which looks like a Jira issue"
+    msg = Contracts::HipchatMessage.new(build_test_json(text))
+    assert_true(msg.hasJiraIssue)
+  end
+
+  def test_jira_issue_should_be_extracted_when_present
+    text = "Message containing THX-1138, which looks like a Jira issue"
+    msg = Contracts::HipchatMessage.new(build_test_json(text))
+    assert_equal(msg.jiraIssues, ["THX-1138"])
+  end
+
+  def test_multiple_jira_issues_should_be_found_in_the_message
+    text = "DEP-31337 is the deploy task for THX-1138"
+    msg = Contracts::HipchatMessage.new(build_test_json(text))
+    assert_equal(msg.jiraIssues, ["DEP-31337", "THX-1138"])
+  end
+
 end
